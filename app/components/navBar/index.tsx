a39a02c1897f5e86a8b026c9d05226dd1a56ddcb
Kelvin Mijaya <kelvinmijaya@hotmail.com>
2024-01-03 16:51:29 +0700

    fix list key

--- app/components/navBar/index.tsx
+++ app/components/navBar/index.tsx
@@ -62,9 +62,9 @@ export default function NavBar() {
         ></div>
         <nav className="pointer-event-auto hidden md:block">
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-            {navList.map((list: navigationList) => {
+            {navList.map((list: navigationList, i) => {
               return (
-                <li>
+                <li key={`nav-list-${i}`}>
                   <Link
                     className={`relative block px-3 py-2 transition ${
                       list.path === pathname
