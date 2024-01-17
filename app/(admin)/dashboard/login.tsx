dffb824d17c6108ccfcdafa8b79294537e94a47f
Kelvin Mijaya <kelvinmijaya@hotmail.com>
2024-01-18 06:09:08 +0700

    add route group/organization

---
+++ app/(admin)/dashboard/login.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div className="text-sm text-zinc-400">login ni</div>
+}
---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Dashboard() {
+  return <div className="text-sm text-zinc-400">dashboard ni</div>
+}
