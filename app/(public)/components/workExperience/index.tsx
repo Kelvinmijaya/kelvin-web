e0d673dee6fb56952e8e4c8bb58dd5d67559b7cc
Kelvin Mijaya <kelvinmijaya@gmail.com>
2024-05-21 16:14:05 +0700

    Remove cv from my site

--- app/(public)/components/workExperience/index.tsx
+++ app/(public)/components/workExperience/index.tsx
@@ -1,4 +1,4 @@
-import type {NextComponentType, NextPageContext} from 'next'
+import type { NextComponentType, NextPageContext } from 'next'
 import Image from 'next/image'
 
 // Hooks
@@ -84,26 +84,6 @@ const Index: NextComponentType<NextPageContext> = () => {
           )
         })}
       </ol>
-      <a
-        className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-        href="/Kelvin-CV.pdf"
-        download="cv"
-      >
-        Download CV
-        <svg
-          viewBox="0 0 16 16"
-          fill="none"
-          aria-hidden="true"
-          className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50"
-        >
-          <path
-            d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
-      </a>
     </div>
   )
 }
