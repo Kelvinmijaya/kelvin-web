8a5ed272cfca4fd1b850da1f23bf183ebb214543
Kelvin Mijaya <kelvinmijaya@gmail.com>
2024-05-21 16:11:35 +0700

    Fix typo and vulnerability

--- app/(public)/gear/page.tsx
+++ app/(public)/gear/page.tsx
@@ -1,7 +1,7 @@
-import type {NextPage} from 'next'
+import type { NextPage } from 'next'
 import Image from 'next/image'
 
-import {Title} from '@shared/components/Title'
+import { Title } from '@shared/components/Title'
 
 import Laptop from './images/laptop.jpg'
 import PC from './images/pc.jpg'
@@ -12,24 +12,24 @@ type SpecType = {
 }
 
 const pcSpec: SpecType[] = [
-  {name: 'Main Monitor', value: 'Samsung Oddyssey G7 27 inch'},
-  {name: 'Secondary Monitor', value: 'LG Ultragear 27 inch (Vertical)'},
-  {name: 'PC Case', value: 'Lian li o11 dynamic mini'},
-  {name: 'CPU', value: 'Ryzen 5 5600x + NZXT kraken x63'},
-  {name: 'GPU', value: 'Radeon Sapphire rx 6800-xt nitro+'},
-  {name: 'Motherboard', value: 'MSI mag z570 tomhawk wifi'},
-  {name: 'Ram', value: 'Samsung Oddyssey G7 27 inch'},
-  {name: 'SSD', value: 'Corsair NVME mp600 1 TB'},
-  {name: 'PSU', value: 'Cooler Master V850 SFX Gold'},
-  {name: 'Fan', value: 'Corsair QL210'},
+  { name: 'Main Monitor', value: 'Samsung Oddyssey G7 27 inch' },
+  { name: 'Secondary Monitor', value: 'LG Ultragear 27 inch (Vertical)' },
+  { name: 'PC Case', value: 'Lian li o11 dynamic mini' },
+  { name: 'CPU', value: 'Ryzen 5 5600x + NZXT kraken x63' },
+  { name: 'GPU', value: 'Radeon Sapphire rx 6800-xt nitro+' },
+  { name: 'Motherboard', value: 'MSI mag z570 tomhawk wifi' },
+  { name: 'Ram', value: 'Team XTREEM 64GB 3600mhz - Dual Channel' },
+  { name: 'SSD', value: 'Corsair NVME mp600 1 TB' },
+  { name: 'PSU', value: 'Cooler Master V850 SFX Gold' },
+  { name: 'Fan', value: 'Corsair QL210' }
 ]
 
 const laptopSpec: SpecType[] = [
-  {name: 'Hardware', value: 'Macbook Pro 2024'},
-  {name: 'Color', value: 'Space Black'},
-  {name: 'Processor', value: 'Apple M3 Pro (CPU 12-core & GPU 18-core)'},
-  {name: 'Ram', value: '36GB'},
-  {name: 'SSD', value: '1TB'},
+  { name: 'Hardware', value: 'Macbook Pro 2024' },
+  { name: 'Color', value: 'Space Black' },
+  { name: 'Processor', value: 'Apple M3 Pro (CPU 12-core & GPU 18-core)' },
+  { name: 'Ram', value: '36GB' },
+  { name: 'SSD', value: '1TB' }
 ]
 
 const Gear: NextPage = () => {
@@ -126,43 +126,6 @@ const Gear: NextPage = () => {
       </div>
     </div>
   )
-  // return (
-  //   <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12 sm:gap-y-8">
-  //     <div className="lg:pl-20">
-  //       <div className="px-2.5 lg:max-w-none">
-  //         <Title>PC</Title>
-  //         <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-  //           \
-  //           {/* <Image
-  //           alt="kelvin mijaya portrait"
-  //           className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-  //           src={ImgPortrait}
-  //           width={640}
-  //           height={640}
-  //           priority={true}
-  //         /> */}
-  //           <p>asd</p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div className="lg:order-first lg:row-span-2">
-  //       <div className="px-2.5 lg:max-w-none">
-  //         <Title>Laptop</Title>
-  //         <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
-  //           {/* <Image
-  //           alt="kelvin mijaya portrait"
-  //           className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-  //           src={ImgPortrait}
-  //           width={640}
-  //           height={640}
-  //           priority={true}
-  //         /> */}
-  //           <p>asd</p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // )
 }
 
 export default Gear
